f1cbf43c2041e53cc4de4ff51348bc24da3119ab
Jason Young <jasonyoung@google.com>
2017-07-19 10:33:22 -0700

    add //pkg/client/file for partial file decoding (#58)
    Add a helper package to do partial decoding of user configuration
    files. This is necessary to contruct the `path` from the encoded
    `scope` and `path`. Also try to properly detect the content-type.

---
+++ pkg/client/file/file.go
@@ -0,0 +1,130 @@
+// Copyright 2017 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package file
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+
+	galleypb "istio.io/galley/api/galley/v1"
+
+	"github.com/ghodss/yaml"
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes/struct"
+)
+
+// File provides a container for common file request and response
+// operations as described by istio.galley.v1.File services.
+type File struct {
+	Path        string
+	Revision    int64
+	Contents    []byte
+	ContentType galleypb.ContentType
+
+	// Name and Scope are extracted from the decoded contents
+	Name  string
+	Scope string
+}
+
+// structure to allow for partial decoding of file contents to
+// discover name and scope.
+type partialConfigFile struct {
+	Name   string        `json:"name"`
+	Scope  string        `json:"scope"`
+	Config []interface{} `json:"config"`
+}
+
+func loadFileAsJSON(in []byte) (*partialConfigFile, error) {
+	var pcf partialConfigFile
+	if err := json.Unmarshal(in, &pcf); err != nil {
+		return nil, err
+	}
+	return &pcf, nil
+}
+
+func loadFileAsYAML(in []byte) (*partialConfigFile, error) {
+	var pcf partialConfigFile
+	if err := yaml.Unmarshal(in, &pcf); err != nil {
+		return nil, err
+	}
+	return &pcf, nil
+}
+
+func loadFileAsProto(in []byte) (*partialConfigFile, error) {
+	var pb structpb.Struct
+	if err := proto.Unmarshal(in, &pb); err != nil {
+		return nil, err
+	}
+	return &partialConfigFile{
+		Name:  pb.GetFields()["name"].GetStringValue(),
+		Scope: pb.GetFields()["scope"].GetStringValue(),
+	}, nil
+}
+
+// PartialDecode performs a partial decoding of the user specified
+// configuration file. It returns a file container which includes the
+// decoded name and scope along with the detected content-type.
+func PartialDecode(contents []byte, ctype galleypb.ContentType) (*File, error) {
+	var pcf *partialConfigFile
+	var err error
+	switch ctype {
+	case galleypb.ContentType_JSON:
+		if pcf, err = loadFileAsJSON(contents); err != nil {
+			return nil, fmt.Errorf("cannot decode as json: %v", err)
+		}
+	case galleypb.ContentType_YAML:
+		if pcf, err = loadFileAsYAML(contents); err != nil {
+			return nil, fmt.Errorf("cannot decode as yaml: %v", err)
+		}
+	case galleypb.ContentType_PROTO_TEXT:
+		if pcf, err = loadFileAsProto(contents); err != nil {
+			return nil, fmt.Errorf("cannot decode as prototext: %v", err)
+		}
+	default:
+		// Guess the content-type. The order is important here. YAML
+		// is a superset of JSON so try JSON first.
+		if pcf, err = loadFileAsJSON(contents); err == nil {
+			ctype = galleypb.ContentType_JSON
+			break
+		}
+		if pcf, err = loadFileAsYAML(contents); err == nil {
+			ctype = galleypb.ContentType_YAML
+			break
+		}
+		if pcf, err = loadFileAsProto(contents); err == nil {
+			ctype = galleypb.ContentType_PROTO_TEXT
+			break
+		}
+		return nil, errors.New("unknown content type: use --ctype or specify path explicitly with --path")
+	}
+	return &File{
+		Name:        pcf.Name,
+		Scope:       pcf.Scope,
+		Contents:    contents,
+		ContentType: ctype,
+	}, nil
+}
+
+// PartialDecodeFromFilename is a helper function to partially decode a user
+// specified configuration file based on its name.
+func PartialDecodeFromFilename(filename string, ctype galleypb.ContentType) (*File, error) {
+	contents, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return PartialDecode(contents, ctype)
+}
---
+++ pkg/client/file/file_test.go
@@ -0,0 +1,166 @@
+// Copyright 2017 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package file
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+
+	galleypb "istio.io/galley/api/galley/v1"
+
+	"github.com/ghodss/yaml"
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes/struct"
+)
+
+func TestPartialDecode(t *testing.T) {
+	wantName := "com.example"
+	wantScope := "service.cfg"
+	wantContent := map[string]interface{}{
+		"scope": wantScope,
+		"name":  wantName,
+		"config": []map[string]interface{}{
+			{
+				"type": "route-rule",
+				"spec": "...",
+			},
+		},
+	}
+
+	wantContentJSON, err := json.Marshal(&wantContent)
+	if err != nil {
+		t.Fatalf("could not create JSON test content: %v", err)
+	}
+	wantContentYAML, err := yaml.Marshal(&wantContent)
+	if err != nil {
+		t.Fatalf("could not create YAML test content: %v", err)
+	}
+	var pb structpb.Struct
+	if err := jsonpb.UnmarshalString(string(wantContentJSON), &pb); err != nil {
+		t.Fatalf("could not create temp protobuf for ProtoText test content: %v", err)
+	}
+	wantContentProto, err := proto.Marshal(&pb)
+	if err != nil {
+		t.Fatalf("could not create ProtoText test content: %v", err)
+	}
+
+	cases := []struct {
+		name      string
+		content   []byte
+		ctype     galleypb.ContentType
+		wantCtype galleypb.ContentType
+
+		wantErr bool
+	}{
+		{
+			name:      "JSON as JSON",
+			content:   wantContentJSON,
+			ctype:     galleypb.ContentType_JSON,
+			wantCtype: galleypb.ContentType_JSON,
+		},
+		{
+			name:      "JSON as YAML",
+			content:   wantContentJSON,
+			ctype:     galleypb.ContentType_YAML,
+			wantCtype: galleypb.ContentType_YAML,
+		},
+		{
+			name:    "JSON as PROTO",
+			content: wantContentJSON,
+			ctype:   galleypb.ContentType_PROTO_TEXT,
+			wantErr: true,
+		},
+		{
+			name:      "JSON as UNKNOWN",
+			content:   wantContentJSON,
+			ctype:     galleypb.ContentType_UNKNOWN,
+			wantCtype: galleypb.ContentType_JSON,
+		},
+		{
+			name:      "YAML as JSON",
+			content:   wantContentYAML,
+			ctype:     galleypb.ContentType_JSON,
+			wantCtype: galleypb.ContentType_JSON,
+			wantErr:   true,
+		},
+		{
+			name:      "YAML as YAML",
+			content:   wantContentYAML,
+			ctype:     galleypb.ContentType_YAML,
+			wantCtype: galleypb.ContentType_YAML,
+		},
+		{
+			name:    "YAML as PROTO",
+			content: wantContentYAML,
+			ctype:   galleypb.ContentType_PROTO_TEXT,
+			wantErr: true,
+		},
+		{
+			name:      "YAML as UNKNOWN",
+			content:   wantContentYAML,
+			ctype:     galleypb.ContentType_UNKNOWN,
+			wantCtype: galleypb.ContentType_YAML,
+		},
+		{
+			name:    "PROTO as JSON",
+			content: wantContentProto,
+			ctype:   galleypb.ContentType_JSON,
+			wantErr: true,
+		},
+		{
+			name:    "PROTO as YAML",
+			content: wantContentProto,
+			ctype:   galleypb.ContentType_YAML,
+			wantErr: true,
+		},
+		{
+			name:      "PROTO as PROTO",
+			content:   wantContentProto,
+			ctype:     galleypb.ContentType_PROTO_TEXT,
+			wantCtype: galleypb.ContentType_PROTO_TEXT,
+		},
+		{
+			name:      "PROTO as UNKNOWN",
+			content:   wantContentProto,
+			ctype:     galleypb.ContentType_UNKNOWN,
+			wantCtype: galleypb.ContentType_PROTO_TEXT,
+		},
+	}
+
+	for _, c := range cases {
+		gotFile, gotErr := PartialDecode(c.content, c.ctype)
+		expected := gotErr != nil
+		if expected != c.wantErr {
+			t.Errorf("%v: wrong error value: got %v want %v", c.name, expected, c.wantErr)
+		}
+		if gotErr != nil {
+			continue
+		}
+		if gotFile.Name != wantName {
+			t.Errorf("%v: wrong name  got %v want %v", c.name, gotFile.Name, wantName)
+		}
+		if gotFile.Scope != wantScope {
+			t.Errorf("%v: wrong scope: got %v want %v", c.name, gotFile.Scope, wantScope)
+		}
+		if !reflect.DeepEqual(gotFile.Contents, c.content) {
+			t.Errorf("%v: wrong contents: got %v want %v", c.name, gotFile.Contents, c.content)
+		}
+		if gotFile.ContentType != c.wantCtype {
+			t.Errorf("%v: wrong content-type: got %v want %v", c.name, gotFile.ContentType, c.wantCtype)
+		}
+	}
+}
